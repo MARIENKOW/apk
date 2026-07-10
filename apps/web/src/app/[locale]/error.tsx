8c2013e5b731f7249b495639126ad68dc8e8cc93
MARIENKOW <den.marienkow@gmail.com>
2026-07-11 02:20:17 +0300

    fix

--- apps/web/src/app/[locale]/error.tsx
+++ apps/web/src/app/[locale]/error.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import ErrorElement from "@/components/feedback/error/ErrorElement";
-
+console.log('');
 export default function ErrorPage({ error }: { error: Error }) {
     return <ErrorElement message={error?.message} />;
 }
